e6520d9b6906a8b53c4db2cb01b5dc145d1a99af
Arne-Zillhardt <arne_zillhardt@outlook.com>
2023-02-26 19:40:23 +0100

    Version 1.0: Release

--- src/com/company/Calculation/Calculator.java
+++ src/com/company/Calculation/Calculator.java
@@ -7,29 +7,23 @@ public class Calculator {
     ArrayList<Double> results = new ArrayList<>();
 
     public void getResults(double rating1, double rating2) {
-        probebility(rating1, rating2);
-
         //0
-        win(rating1, results.get(0)); //Chance of winning
+        probebility(rating1, rating2); //Chance of winning
+
         //1
-        results.add(results.get(0) + rating1); //If win, new score
+        win(rating1, results.get(0)); //If win, new score
         //2
-        results.set(2, results.get(1) - rating1); //If win, scorechange
+        results.add(results.get(1) - rating1); //If win, scorechange
 
         //3
-        draw(rating1, results.get(0));
+        draw(rating1, results.get(0)); //If draw, new score
         //4
-        results.add(results.get(3) + rating1);
+        results.add(results.get(3) - rating1); //If draw, scorechange
+
         //5
-        results.set(5, results.get(4) - rating1);
-        String s = "Test" +
-                    " dskfn";
+        loose(rating1, results.get(0)); //If loose, new score
         //6
-        loose(rating1, results.get(0));
-        //7
-        results.add(results.get(6) + rating1);
-        //8
-        results.set(8, results.get(7) - rating1);
+        results.add(results.get(5) - rating1); //If loose, scorechange
     }
 
     void probebility(double r1, double r2) {
--- src/com/company/InputOutput/Output.java
+++ src/com/company/InputOutput/Output.java
@@ -16,18 +16,18 @@ public class Output {
 
         System.out.println("If you win, you gain "  + res.get(2) + " points and your rating would be "  + res.get(1));
 
-        if (res.get(5) > 0) {
-            System.out.println("If you draw, you gain "  + res.get(5) + " points and your rating would be "  + res.get(4));
-        } else if (res.get(5) == 0) {
-            System.out.println("If you draw, your rating stays the same and would be " +  + res.get(4));
+        if (res.get(4) > 0) {
+            System.out.println("If you draw, you gain "  + res.get(4) + " points and your rating would be "  + res.get(3));
+        } else if (res.get(4) == 0) {
+            System.out.println("If you draw, your rating stays the same and would be " + res.get(3));
         } else {
-            System.out.println("If you draw, you loose "  + res.get(5) + " points and your rating would be " +  + res.get(4));
+            System.out.println("If you draw, you loose "  + res.get(4) + " points and your rating would be " + res.get(3));
         }
 
-        if (res.get(8) == 0) {
-            System.out.println("If you loose, your rating stays the same and would be "  + res.get(7));
+        if (res.get(6) == 0) {
+            System.out.println("If you loose, your rating stays the same and would be "  + res.get(5));
         } else {
-            System.out.println("If you loose, you loose "  + res.get(8) + "points and your rating would be "  + res.get(7));
+            System.out.println("If you loose, you loose "  + res.get(6) + "points and your rating would be "  + res.get(5));
         }
     }
 }
